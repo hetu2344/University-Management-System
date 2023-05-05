b194027522b1c12ade38cecd99985bd69beaaaec
Het Patel <108039058+hetu2344@users.noreply.github.com>
2023-05-05 17:16:20 -0500

    Add files via upload

---
+++ java/Instructor/AddInstructor.java
@@ -0,0 +1,182 @@
+package java.Instructor;
+
+import com.toedter.calendar.JDateChooser;
+
+import javax.swing.*;
+import java.Database.Conn;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class AddInstructor extends JFrame implements ActionListener {
+
+    private Conn conn;
+
+    private Random ran = new Random();
+    private long last6 = Math.abs((ran.nextLong() % 900000L) + 100000L);
+
+    private JTextField tfFirstName, tfMiddleName, tfLastName, tdAddress, tfMobile, tfEmail;
+    private JLabel lblEmpId;
+    private JButton addInstructor = new JButton("Add Instructor"), cancel = new JButton("Cancel"), clear = new JButton("Clear");
+    private JDateChooser dtDob;
+    private JComboBox txtFaculty;
+
+
+    public AddInstructor(Conn c) {
+
+        this.conn = c;
+
+        //Getting dimension of screen
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        final int WIDTH = screenSize.width, LENGTH = screenSize.height;
+
+        setLayout(null);
+
+        JLabel heading = new JLabel("New Instructor Details");
+        heading.setBounds(275, 10, 250, 30);
+        heading.setFont(new Font("serif", Font.BOLD, 25));
+        add(heading);
+
+        JLabel FirstName = new JLabel("First Name");
+        FirstName.setBounds(50, 60, 100, 30);
+        FirstName.setFont(new Font("serif", Font.BOLD, 20));
+        add(FirstName);
+
+        tfFirstName = new JTextField();
+        tfFirstName.setBounds(175, 60, 100, 30);
+        add(tfFirstName);
+
+        JLabel middleName = new JLabel("Middle Name");
+        middleName.setBounds(425, 60, 130, 30);
+        middleName.setFont(new Font("serif", Font.BOLD, 20));
+        add(middleName);
+
+        tfMiddleName = new JTextField();
+        tfMiddleName.setBounds(570, 60, 100, 30);
+        add(tfMiddleName);
+
+        JLabel lastName = new JLabel("Last Name");
+        lastName.setBounds(50, 120, 130, 30);
+        lastName.setFont(new Font("serif", Font.BOLD, 20));
+        add(lastName);
+
+        tfLastName = new JTextField();
+        tfLastName.setBounds(175, 120, 100, 30);
+        add(tfLastName);
+
+        JLabel txtID = new JLabel("Employee Number");
+        txtID.setBounds(425, 120, 150, 30);
+        txtID.setFont(new Font("serif", Font.BOLD, 20));
+        add(txtID);
+
+        lblEmpId = new JLabel("1403" + last6);
+        lblEmpId.setBounds(590, 120, 100, 30);
+        add(lblEmpId);
+
+
+        JLabel dob = new JLabel("Date of Birth");
+        dob.setBounds(50, 180, 130, 30);
+        dob.setFont(new Font("serif", Font.BOLD, 20));
+        add(dob);
+
+        dtDob = new JDateChooser();
+        dtDob.setBounds(175, 180, 150, 30);
+        add(dtDob);
+
+        JLabel address = new JLabel("Address");
+        address.setBounds(425, 180, 100, 30);
+        address.setFont(new Font("serif", Font.BOLD, 20));
+        add(address);
+
+        tdAddress = new JTextField();
+        tdAddress.setBounds(510, 180, 200, 50);
+        add(tdAddress);
+
+        JLabel mobile = new JLabel("Phone Number");
+        mobile.setBounds(50, 250, 130, 30);
+        mobile.setFont(new Font("serif", Font.BOLD, 20));
+        add(mobile);
+
+        tfMobile = new JTextField();
+        tfMobile.setBounds(175, 250, 150, 30);
+        add(tfMobile);
+
+        JLabel faculty = new JLabel("Faculty");
+        faculty.setBounds(425, 250, 100, 30);
+        faculty.setFont(new Font("serif", Font.BOLD, 20));
+        add(faculty);
+
+        String[] faculties = {"Science", "Engineering", "Humanities", "Social Work", "Arts", "Agriculture", "Architecture"};
+        txtFaculty = new JComboBox(faculties);
+        txtFaculty.setBounds(510, 240, 200, 50);
+        add(txtFaculty);
+
+        JLabel email = new JLabel("E-mail");
+        email.setBounds(50, 310, 130, 30);
+        email.setFont(new Font("serif", Font.BOLD, 20));
+        add(email);
+
+        tfEmail = new JTextField();
+        tfEmail.setBounds(175, 310, 150, 30);
+        add(tfEmail);
+
+        addInstructor.setBounds(100, 410, 120, 40);
+        addInstructor.addActionListener(this);
+        add(addInstructor);
+
+        clear.setBounds(300, 410, 120, 40);
+        clear.addActionListener(this);
+        add(clear);
+
+        cancel.setBounds(500, 410, 120, 40);
+        cancel.addActionListener(this);
+        add(cancel);
+        //Setting location and size of window
+        setResizable(false);
+        setSize(775, 500);
+        setLocation((WIDTH - 800) / 2, (LENGTH - 600) / 2);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == addInstructor) {
+            String FName = tfFirstName.getText();
+            String MName = tfMiddleName.getText();
+            String LName = tfLastName.getText();
+            String empID = lblEmpId.getText();
+            String dob = ((JTextField) (dtDob.getDateEditor().getUiComponent())).getText();
+            String address = tdAddress.getText();
+            String mobile = tfMobile.getText();
+            String email = tfEmail.getText();
+            String faculty = (String) (txtFaculty.getSelectedItem());
+            try{
+                String query = "INSERT INTO instructor values('" + FName + "', '" + MName + "', '" + LName + "', '" + empID + "', '" + dob + "', '" + address + "', '" + mobile + "', '" + email + "', '" + faculty + "');";
+                conn.stmt.executeUpdate(query);
+
+                JOptionPane.showMessageDialog(null, "Instructor details added successfully");
+
+                tfFirstName.setText("");
+                tfMiddleName.setText("");
+                tfLastName.setText("");
+                tdAddress.setText("");
+                tfMobile.setText("");
+                tfEmail.setText("");
+
+            } catch(Exception ex){
+                ex.printStackTrace();
+            }
+
+        } else if(e.getSource() == clear){
+            tfFirstName.setText("");
+            tfMiddleName.setText("");
+            tfLastName.setText("");
+            tdAddress.setText("");
+            tfMobile.setText("");
+            tfEmail.setText("");
+        } else if (e.getSource() == cancel){
+            setVisible(false);
+        }
+    }
+}
---
+++ java/Student/UpdateStudent.java
@@ -0,0 +1,245 @@
+package java.Student;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.ResultSet;
+import java.Database.Conn;
+
+
+public class UpdateStudent extends JFrame implements ActionListener {
+
+    Conn conn;
+
+    Choice cStuNum;
+
+    String stuNo;
+
+    JTextField tdAddress, tfMobile, tfEmail, txtFaculty;
+    JLabel lblSNumber, tfFirstName, tfMiddleName, tfLastName, tfDOB;
+    JButton updateStudent = new JButton("Update java.Student"), cancel = new JButton("Cancel"), select = new JButton("Select");
+
+    public UpdateStudent(String stuNo, Conn c) {
+
+        this.conn = c;
+
+        setLayout(null);
+
+        initializeLabels();
+
+        initializeButtons();
+
+        initializeFrame();
+
+        changeLabels(stuNo);
+    }
+
+    public UpdateStudent(Conn c) {
+
+        this.conn = c;
+
+        setLayout(null);
+
+        initializeLabels();
+
+        initializeButtons();
+
+        initializeFrame();
+    }
+
+    private void initializeFrame() {
+
+        //Getting dimension of screen
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        final int WIDTH = screenSize.width, LENGTH = screenSize.height;
+
+        //Setting location and size of window
+        setTitle("Update java.Student Details");
+        setResizable(false);
+        setSize(775, 500);
+        setLocation((WIDTH - 800) / 2, (LENGTH - 600) / 2);
+        setVisible(true);
+    }
+
+    private void initializeButtons() {
+        updateStudent.setBounds(100, 410, 120, 40);
+        updateStudent.addActionListener(this);
+        add(updateStudent);
+
+        select.setBounds(500, 20, 120, 30);
+        select.addActionListener(this);
+        add(select);
+
+        cancel.setBounds(500, 410, 120, 40);
+        cancel.addActionListener(this);
+        add(cancel);
+    }
+
+    private void initializeLabels() {
+        JLabel selectNo = new JLabel("Select by java.Student Number");
+        selectNo.setBounds(20, 20, 180, 30);
+        add(selectNo);
+
+        cStuNum = new Choice();
+        cStuNum.setBounds(210, 20, 150, 30);
+        add(cStuNum);
+
+        try {
+            ResultSet rs = conn.stmt.executeQuery("SELECT studentNo FROM student");
+            while(rs.next()) {
+                cStuNum.add(rs.getString("studentNo"));
+            }
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+
+        JLabel FirstName = new JLabel("First Name");
+        FirstName.setBounds(50, 60, 100, 30);
+        FirstName.setFont(new Font("serif", Font.BOLD, 20));
+        add(FirstName);
+
+        tfFirstName = new JLabel();
+        tfFirstName.setFont(new Font("serif", Font.PLAIN, 20));
+        tfFirstName.setBounds(175, 60, 100, 30);
+        add(tfFirstName);
+
+        JLabel middleName = new JLabel("Middle Name");
+        middleName.setBounds(425, 60, 130, 30);
+        middleName.setFont(new Font("serif", Font.BOLD, 20));
+        add(middleName);
+
+        tfMiddleName = new JLabel();
+        tfMiddleName.setFont(new Font("serif", Font.PLAIN, 20));
+        tfMiddleName.setBounds(570, 60, 100, 30);
+        add(tfMiddleName);
+
+        JLabel lastName = new JLabel("Last Name");
+        lastName.setBounds(50, 120, 130, 30);
+        lastName.setFont(new Font("serif", Font.BOLD, 20));
+        add(lastName);
+
+        tfLastName = new JLabel();
+        tfLastName.setFont(new Font("serif", Font.PLAIN, 20));
+        tfLastName.setBounds(175, 120, 100, 30);
+        add(tfLastName);
+
+        JLabel txtNumber = new JLabel("java.Student Number");
+        txtNumber.setBounds(425, 120, 150, 30);
+        txtNumber.setFont(new Font("serif", Font.BOLD, 20));
+        add(txtNumber);
+
+        lblSNumber = new JLabel();
+        lblSNumber.setBounds(600, 120, 100, 30);
+        lblSNumber.setFont(new Font("serif", Font.PLAIN, 20));
+        add(lblSNumber);
+
+
+        JLabel dob = new JLabel("Date of Birth");
+        dob.setBounds(50, 180, 130, 30);
+        dob.setFont(new Font("serif", Font.BOLD, 20));
+        add(dob);
+
+        tfDOB = new JLabel();
+        tfDOB.setBounds(175, 180, 100, 30);
+        add(tfDOB);
+
+
+        JLabel address = new JLabel("Address");
+        address.setBounds(425, 180, 100, 30);
+        address.setFont(new Font("serif", Font.BOLD, 20));
+        add(address);
+
+        tdAddress = new JTextField();
+        tdAddress.setBounds(510, 180, 200, 50);
+        add(tdAddress);
+
+        JLabel mobile = new JLabel("Phone Number");
+        mobile.setBounds(50, 250, 130, 30);
+        mobile.setFont(new Font("serif", Font.BOLD, 20));
+        add(mobile);
+
+        tfMobile = new JTextField();
+        tfMobile.setBounds(200, 250, 170, 30);
+        add(tfMobile);
+
+        JLabel faculty = new JLabel("Faculty");
+        faculty.setBounds(425, 250, 100, 30);
+        faculty.setFont(new Font("serif", Font.BOLD, 20));
+        add(faculty);
+
+        txtFaculty = new JTextField();
+        txtFaculty.setBounds(510, 250, 170, 30);
+        add(txtFaculty);
+
+
+        JLabel email = new JLabel("E-mail");
+        email.setBounds(50, 310, 130, 30);
+        email.setFont(new Font("serif", Font.BOLD, 20));
+        add(email);
+
+        tfEmail = new JTextField();
+        tfEmail.setBounds(200, 310, 170, 30);
+        add(tfEmail);
+    }
+
+    private void changeLabels(String stuNo) {
+        try {
+            cStuNum.select(stuNo);
+            String query = "SELECT * FROM student WHERE studentNo = '" + stuNo + "'";
+            ResultSet rs = conn.stmt.executeQuery(query);
+            rs.next();
+            tfFirstName.setText(rs.getString(1));
+            tfMiddleName.setText(rs.getString(2));
+            tfLastName.setText(rs.getString(3));
+            lblSNumber.setText(rs.getString(4));
+            tfDOB.setText(rs.getString(5));
+            tdAddress.setText(rs.getString(6));
+            tfMobile.setText(rs.getString(7));
+            tfEmail.setText(rs.getString(8));
+            txtFaculty.setText(rs.getString(9));
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == updateStudent) {
+            String studentNumber = lblSNumber.getText();
+            String address = tdAddress.getText();
+            String mobile = tfMobile.getText();
+            String email = tfEmail.getText();
+            String faculty = txtFaculty.getText();
+            try {
+
+                String query = "UPDATE student set address = '" + address + "', mobile = '" + mobile + "', email = '" + email + "', faculty = '" + faculty + "' WHERE studentNo = '" + studentNumber + "'";
+
+                conn.stmt.executeUpdate(query);
+
+                JOptionPane.showMessageDialog(null, "java.Student details updated successfully");
+
+                tdAddress.setText("");
+                tfMobile.setText("");
+                txtFaculty.setText("");
+                tfEmail.setText("");
+
+            } catch(Exception ex) {
+                ex.printStackTrace();
+            }
+
+        } else if(e.getSource() == select) {
+            tdAddress.setText("");
+            tfMobile.setText("");
+            tfEmail.setText("");
+            txtFaculty.setText("");
+
+            this.stuNo = cStuNum.getSelectedItem();
+
+            changeLabels(stuNo);
+        } else if(e.getSource() == cancel) {
+            setVisible(false);
+            this.dispose();
+        }
+    }
+}
